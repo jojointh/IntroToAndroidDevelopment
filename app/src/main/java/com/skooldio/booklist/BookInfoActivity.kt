06840985eb7747005872f96463ce4fc527dba3ee
Akexorcist <akexorcist@gmail.com>
2019-06-15 03:33:36 +0700

    Go to book info screen with filling book info from main screen

--- app/src/main/java/com/skooldio/booklist/BookInfoActivity.kt
+++ app/src/main/java/com/skooldio/booklist/BookInfoActivity.kt
@@ -2,11 +2,25 @@ package com.skooldio.booklist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_book_info.*
+import kotlinx.android.synthetic.main.activity_main.textViewAuthor
+import kotlinx.android.synthetic.main.activity_main.textViewTitle
 
 class BookInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_info)
+
+        val bundle = intent
+        val isbn = bundle.getStringExtra("isbn")
+        val title = bundle.getStringExtra("title")
+        val author = bundle.getStringExtra("author")
+        val pages = bundle.getIntExtra("pages", 0)
+
+        textViewIsbn.text = isbn
+        textViewTitle.text = title
+        textViewAuthor.text = author
+        textViewPages.text = "$pages"
     }
 }
--- app/src/main/java/com/skooldio/booklist/MainActivity.kt
+++ app/src/main/java/com/skooldio/booklist/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.skooldio.booklist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAdd.setOnClickListener {
+            val isbn = editTextIsbn.text.toString()
+            val title = editTextTitle.text.toString()
+            val author = editTextAuthor.text.toString()
+            val pages = editTextPages.text.toString().toIntOrNull() ?: 0
+
+            val intent = Intent(this, BookInfoActivity::class.java)
+            intent.putExtra("isbn", isbn)
+            intent.putExtra("title", title)
+            intent.putExtra("author", author)
+            intent.putExtra("pages", pages)
+            startActivity(intent)
+        }
     }
 }
