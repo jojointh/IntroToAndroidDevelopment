0b4cb96d55dcd98477c67c0994075b81cef51034
Akexorcist <akexorcist@gmail.com>
2019-06-15 03:02:43 +0700

    Create book info activity and layout

---
+++ app/src/main/java/com/skooldio/booklist/BookInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.skooldio.booklist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BookInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_info)
+    }
+}
