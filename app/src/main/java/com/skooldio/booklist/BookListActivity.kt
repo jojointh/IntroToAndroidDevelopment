753612addf8cb81f9942898e5539a7e1fd994354
Akexorcist <akexorcist@gmail.com>
2019-06-15 04:30:08 +0700

    Add to do for step 8

--- app/src/main/java/com/skooldio/booklist/BookListActivity.kt
+++ app/src/main/java/com/skooldio/booklist/BookListActivity.kt
@@ -8,5 +8,9 @@ class BookListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_list)
+
+        // TODO Create book list
+        // TODO Create adapter with book list then set adapter to RecyclerView
+        // TODO Don't forget to set LayoutManager to RecyclerView
     }
 }
