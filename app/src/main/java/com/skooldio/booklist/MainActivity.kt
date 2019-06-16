2aa6095252b8d4744e85b10ea35124ffdb9e0df0
Akexorcist <akexorcist@gmail.com>
2019-06-16 08:10:52 +0700

    Update new package for book the data class

--- app/src/main/java/com/skooldio/booklist/MainActivity.kt
+++ app/src/main/java/com/skooldio/booklist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.skooldio.booklist
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.skooldio.booklist.vo.Book
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
