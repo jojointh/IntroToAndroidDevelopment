b36c7c7e820497fb5d34378ae612fcbf67dae3e7
Akexorcist <akexorcist@gmail.com>
2019-06-16 08:28:08 +0700

    Add room annotation to book the data class

--- app/src/main/java/com/skooldio/booklist/vo/Book.kt
+++ app/src/main/java/com/skooldio/booklist/vo/Book.kt
@@ -1,12 +1,24 @@
 package com.skooldio.booklist.vo
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "book_list")
 data class Book(
+    @PrimaryKey
+    @ColumnInfo(name = "isbn")
     var isbn: String?,
+
+    @ColumnInfo(name = "title")
     var title: String?,
+
+    @ColumnInfo(name = "author")
     var author: String?,
+
+    @ColumnInfo(name = "pages")
     var pages: Int
 ) : Parcelable
